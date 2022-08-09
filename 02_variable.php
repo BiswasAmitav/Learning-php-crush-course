9979a591c47f11a307d634a15673910b4f2c673b
BiswasAmitav <biswasamit2648@gmail.com>
2022-08-09 19:59:10 +0530

    by this code i learnt php basic variable

---
+++ 02_variable.php
@@ -0,0 +1,45 @@
+<?php
+    //String ..series of charecter ..surrounded by quotes
+
+    //integer whole numbers 
+
+    //Decimal numbers 
+
+    //true or false 
+
+    //Special character , which can be hold more one values 
+
+    //A class 
+
+    //Empty 
+
+
+
+
+
+    $name='Amitav';
+
+    $age=40;
+
+    $haskids=true;
+
+    echo $haskids;
+    echo $age;
+
+    echo $haskids;
+
+    echo $name.' is '.$age.' years old ./ ';
+
+    echo "$name is $age years old . ";
+
+    $x='5' + '5';
+
+    echo $x;
+
+
+    define('HOST','localhost');
+    define('DB_NAME','dev_db');
+
+    echo HOST;
+
+?>
--- first.php
+++ first.php
@@ -1,4 +1,15 @@
 <?php
-echo "My first php programe";
+//echo "My first php programe";
 
+//echo 1; 
+
+//print_r([1,2,3,4]);
+
+
+//var_dump('Hello amit '); //VAR_DUMP IT SHOWES THE VARIABLE TYPE WITH ITS DATA
+//var_dump(true);//IT GIVES US THE BOOLEAN VALUES 
+
+//var_dump('Hello amit i am fine ');
+var_export('hello')
 ?>
+
