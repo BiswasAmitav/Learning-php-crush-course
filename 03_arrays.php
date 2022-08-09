7f16dbfa17e82061c24f22f8b8f9614f4cff4871
BiswasAmitav <biswasamit2648@gmail.com>
2022-08-09 20:33:02 +0530

    by this code i learnt all about php arrays & how to acces it as well as declare

---
+++ 03_arrays.php
@@ -0,0 +1,50 @@
+<?php
+    // $numbers=[1,2,3,4,5,6,7,9];
+
+    // $fruits=array('apple','orange','pineapple');
+
+    // print_r($numbers);
+    // var_dump($fruits);
+    // echo $fruits[2];
+
+    // echo $numbers[0];
+
+
+
+    // echo $fruits[0];
+
+
+
+    //Associative array....
+
+    $color =[
+        1 => 'red',
+        4 => 'blue',
+        6 => 'green'
+    ];
+
+    echo $color[4];
+
+
+    $hex=[
+        'red' => '#f00',
+        'blue' => '#0fo',
+        'green' => '#0f'
+    ];
+
+    echo $hex['blue'];
+
+
+    $person =[
+        ['first_name' => 'dipto',
+        'last_name' => 'Choudhury',
+        'age' => 23],
+        [
+            
+        ]  
+    ];
+
+    echo $person['age'];
+
+
+?>
